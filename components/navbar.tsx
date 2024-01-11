eddd61cbe63fc46e1da90a432d86e14412de2b61
Joshua Chou <joshuachou912@gmail.com>
2024-01-11 16:37:53 -0500

    Moved projects page to home

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <div className="flex space-x-8"> {/* Adjust the space here */}
           <Link href="/" legacyBehavior><a className="text-gray-300 hover:text-white">Home</a></Link>
           <Link href="/about" legacyBehavior><a className="text-gray-300 hover:text-white">About</a></Link>
-          <Link href="/projects" legacyBehavior><a className="text-gray-300 hover:text-white">Projects</a></Link>
+          {/* <Link href="/projects" legacyBehavior><a className="text-gray-300 hover:text-white">Projects</a></Link> */}
           <Link href="/experience" legacyBehavior><a className="text-gray-300 hover:text-white">Experience</a></Link>
           <Link href="/skills" legacyBehavior><a className="text-gray-300 hover:text-white">Skills and Awards</a></Link>
        
